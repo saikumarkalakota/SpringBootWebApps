234953d79e765bf0a2c8af59a5d1843722288bbd
saikumarkalakota <58147758+saikumarkalakota@users.noreply.github.com>
2020-06-08 01:00:59 +0530

    MySprinbootWebApp created

---
+++ src/main/java/com/sai/myapps/MySpringbootWebApp/MySpringbootWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sai.myapps.MySpringbootWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sai/myapps/MySpringbootWebApp/MySpringbootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.myapps.MySpringbootWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
